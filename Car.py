c8d826f4d00a4f58db8ec9c775784ec38dff1cf9
LusineGalstyann <lusinegal8@gmail.com>
2023-08-27 20:37:00 +0400

    Added subclass ElectricCar

--- Car.py
+++ Car.py
@@ -7,8 +7,23 @@ class Car:
         self.color = color
 
     def __str__(self):
-        return f"Make: {self.make}\nModel: {self.model}\nYear: {self.year}\nColor: {self.color}"
+        return f"\nMake: {self.make}\nModel: {self.model}\nYear: {self.year}\nColor: {self.color}"
 
 
-car1 = Car('Toyota', 'Camry', 2020, 'silver')
-print(car1)
+# car1 = Car('Toyota', 'Camry', 2020, 'silver')
+# print(car1)
+
+
+class ElectricCar(Car):
+    def __init__(self,make, model, year, color,kWh):
+        super().__init__(make, model, year, color)
+        self.kWh=kWh
+    def __str__(self):
+        parent=Car.__str__(self)
+        return f"{parent}\nBattery size: {self.kWh}"
+
+elcar1 = ElectricCar('Tesla', 'Model 3', 2022, 'red', 7500)
+elcar2 = ElectricCar('Nissan', 'Leaf', 2021, 'green', 6000)
+
+print(elcar1)
+print(elcar2)
